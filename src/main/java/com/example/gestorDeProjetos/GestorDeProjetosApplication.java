c1ce12d2aa698b8271b0ed954bb0e9af8e570407
KawanHenderson <kawan180706@gmail.com>
2025-11-16 02:02:38 -0300

    initial commit

---
+++ src/main/java/com/example/gestorDeProjetos/GestorDeProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestorDeProjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestorDeProjetos/GestorDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorDeProjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
