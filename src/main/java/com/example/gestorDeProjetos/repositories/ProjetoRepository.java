10fd8c5f90559092489200697979fcba5e8a6128
RickLikeJD <hugojosevaz@gmail.com>
2025-11-19 15:27:13 -0300

    feat: Adicionando repositories

---
+++ src/main/java/com/example/gestorDeProjetos/repositories/ProjetoRepository.java
@@ -0,0 +1,6 @@
+package com.example.gestorDeProjetos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjetoRepository extends JpaRepository <Integer, String> {
+}
