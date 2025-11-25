e098faca82cbe2945c4829713726b08900e07f74
KawanHenderson <kawan180706@gmail.com>
2025-11-25 04:04:27 -0300

    feat: criando repository

--- src/main/java/com/example/gestorDeProjetos/repositories/IssueRepository.java
+++ src/main/java/com/example/gestorDeProjetos/repositories/IssueRepository.java
@@ -1,2 +1,8 @@
 package com.example.gestorDeProjetos.repositories;
 
+import com.example.gestorDeProjetos.models.Issue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IssueRepository extends JpaRepository<Issue, Integer> {}
--- src/main/java/com/example/gestorDeProjetos/repositories/ProjetoRepository.java
+++ src/main/java/com/example/gestorDeProjetos/repositories/ProjetoRepository.java
@@ -1,2 +1,9 @@
 package com.example.gestorDeProjetos.repositories;
 
+
+import com.example.gestorDeProjetos.models.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {}
